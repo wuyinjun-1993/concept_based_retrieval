58b42780a2d91e63bf0d81cb6cc2b102653ff045
wuyinjun <wuyinjun@seas.upenn.edu>
2023-07-01 15:32:07 -0400

    init beir scallop

---
+++ beir/retrieval/search/dense/exact_search.py
@@ -0,0 +1,120 @@
+from .util import cos_sim, dot_score
+import logging
+import sys
+import torch
+from typing import Dict, List
+
+logger = logging.getLogger(__name__)
+
+#Parent class for any dense model
+class DenseRetrievalExactSearch:
+    
+    def __init__(self, model, batch_size: int = 128, corpus_chunk_size: int = 50000, **kwargs):
+        #model is class that provides encode_corpus() and encode_queries()
+        self.model = model
+        self.batch_size = batch_size
+        self.score_functions = {'cos_sim': cos_sim, 'dot': dot_score}
+        self.score_function_desc = {'cos_sim': "Cosine Similarity", 'dot': "Dot Product"}
+        self.corpus_chunk_size = corpus_chunk_size
+        self.show_progress_bar = True #TODO: implement no progress bar if false
+        self.convert_to_tensor = True
+        self.results = {}
+    
+    def search(self, 
+               corpus: Dict[str, Dict[str, str]], 
+               queries: Dict, 
+               top_k: List[int], 
+               score_function: str,
+               return_sorted: bool = False, 
+               query_negations: List=None,
+               **kwargs) -> Dict[str, Dict[str, float]]:
+        #Create embeddings for all queries using model.encode_queries()
+        #Runs semantic search against the corpus embeddings
+        #Returns a ranked list with the corpus ids
+        if score_function not in self.score_functions:
+            raise ValueError("score function: {} must be either (cos_sim) for cosine similarity or (dot) for dot product".format(score_function))
+            
+        logger.info("Encoding Queries...")
+        query_ids = list(queries.keys())
+        self.results = {qid: {} for qid in query_ids}
+        queries = [queries[qid] for qid in query_ids]
+        if query_negations is not None:
+            query_negations = [query_negations[qid] if qid in query_negations else None for qid in query_ids]
+        
+        query_embeddings=[]
+        for idx in range(len(queries)):
+            curr_query = queries[idx]
+            if type(curr_query) is str:
+                curr_query_embedding = self.model.encode_queries(
+                    curr_query, batch_size=self.batch_size, show_progress_bar=self.show_progress_bar, convert_to_tensor=self.convert_to_tensor)
+            elif type(curr_query) is list:
+                curr_query_embedding = []
+                for k in range(len(curr_query)):
+                    qe = self.model.encode_queries(
+                        curr_query[k], batch_size=self.batch_size, show_progress_bar=self.show_progress_bar, convert_to_tensor=self.convert_to_tensor)
+                    curr_query_embedding.append(qe)
+            query_embeddings.append(curr_query_embedding)
+          
+        logger.info("Sorting Corpus by document length (Longest first)...")
+
+        corpus_ids = sorted(corpus, key=lambda k: len(corpus[k].get("title", "") + corpus[k].get("text", "")), reverse=True)
+        corpus = [corpus[cid] for cid in corpus_ids]
+
+        logger.info("Encoding Corpus in batches... Warning: This might take a while!")
+        logger.info("Scoring Function: {} ({})".format(self.score_function_desc[score_function], score_function))
+
+        itr = range(0, len(corpus), self.corpus_chunk_size)
+
+        all_cos_scores = []
+        for batch_num, corpus_start_idx in enumerate(itr):
+            logger.info("Encoding Batch {}/{}...".format(batch_num+1, len(itr)))
+            corpus_end_idx = min(corpus_start_idx + self.corpus_chunk_size, len(corpus))
+
+            #Encode chunk of corpus    
+            sub_corpus_embeddings = self.model.encode_corpus(
+                corpus[corpus_start_idx:corpus_end_idx],
+                batch_size=self.batch_size,
+                show_progress_bar=self.show_progress_bar, 
+                convert_to_tensor = self.convert_to_tensor
+                )
+
+            #Compute similarites using either cosine-similarity or dot product
+            cos_scores = []
+            for query_itr in range(len(query_embeddings)):
+                curr_query_embedding = query_embeddings[query_itr]
+                if type(curr_query_embedding) is list:
+                    curr_cos_scores_ls = self.score_functions[score_function](torch.stack(curr_query_embedding), sub_corpus_embeddings)
+                    if query_negations is not None and query_negations[query_itr] is not None:
+                        curr_query_negations = torch.tensor(query_negations[query_itr])
+                        curr_cos_scores_ls[curr_query_negations == 1] =  - curr_cos_scores_ls[curr_query_negations == 1]
+                    
+                    curr_cos_scores_ls[torch.isnan(curr_cos_scores_ls)] = -1
+                    
+                    curr_cos_scores = 1
+                    for idx in range(len(curr_cos_scores_ls)):
+                        curr_cos_scores *= curr_cos_scores_ls[idx]
+                    
+                    
+                else:
+                    curr_cos_scores = self.score_functions[score_function](curr_query_embedding.unsqueeze(0), sub_corpus_embeddings)
+                    curr_cos_scores[torch.isnan(curr_cos_scores)] = -1
+                    curr_cos_scores = curr_cos_scores.squeeze(0)
+                    
+                cos_scores.append(curr_cos_scores)
+            cos_scores = torch.stack(cos_scores)
+            all_cos_scores.append(cos_scores)
+        
+        all_cos_scores_tensor = torch.cat(all_cos_scores, dim=-1)
+        #Get top-k values
+        cos_scores_top_k_values, cos_scores_top_k_idx = torch.topk(all_cos_scores_tensor, min(top_k+1, len(all_cos_scores_tensor[0])), dim=1, largest=True, sorted=return_sorted)
+        cos_scores_top_k_values = cos_scores_top_k_values.cpu().tolist()
+        cos_scores_top_k_idx = cos_scores_top_k_idx.cpu().tolist()
+        
+        for query_itr in range(len(query_embeddings)):
+            query_id = query_ids[query_itr]                  
+            for sub_corpus_id, score in zip(cos_scores_top_k_idx[query_itr], cos_scores_top_k_values[query_itr]):
+                corpus_id = corpus_ids[sub_corpus_id]
+                if corpus_id != query_id:
+                    self.results[query_id][corpus_id] = score
+        
+        return self.results 
---
+++ explore.py
@@ -0,0 +1,224 @@
+from beir import util, LoggingHandler
+from beir.retrieval import models
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+
+
+import logging
+import pathlib, os
+import os, sys
+
+def intersect_res(results):
+    intersect_keys = set(results[list(results.keys())[0]].keys())
+    for idx in range(len(list(results.keys()))-1):
+        intersect_keys = intersect_keys.intersection(set(results[list(results.keys())[idx + 1]].keys()))
+        
+    intersect_res = {}
+    for key in intersect_keys:
+        score = 1
+        for idx in range(len(list(results.keys()))):
+            score = score*results[list(results.keys())[idx]][key]/100
+        intersect_res[key] = score*100
+        
+    return intersect_res
+
+   
+
+def compare_and_not_query(retriever, corpus, queries, qrels):
+        
+    results = retriever.retrieve(corpus, {"8": queries["8"]})
+
+    print("length of the results::", len(results))
+
+    print("results without decomposition::")
+
+    ndcg, _map, recall, precision = retriever.evaluate({"8": qrels["8"]}, results, retriever.k_values)
+
+    new_results = retriever.retrieve(corpus, {"8": ["lack of testing availability", "underreporting of true incidence of Covid-19"]})
+
+    #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
+    
+
+    new_merged_resuts = intersect_res(new_results)
+
+    print("length of the merged results::", len(new_merged_resuts))
+
+    print("results with decomposition::")
+
+    new_ndcg, new_map, new_recall, new_precision = retriever.evaluate({"8": qrels["8"]}, {"8": new_merged_resuts}, retriever.k_values)
+    
+    new_results2 = retriever.retrieve(corpus, {"8": ["testing availability", "underreporting of true incidence of Covid-19"]}, query_negations={"8":[True,False]})
+
+    #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
+    
+
+    new_merged_resuts2 = intersect_res(new_results2)
+
+    print("length of the merged results::", len(new_merged_resuts2))
+
+    print("results with decomposition::")
+
+    new_ndcg2, new_map2, new_recall2, new_precision2 = retriever.evaluate({"8": qrels["8"]}, {"8": new_merged_resuts2}, retriever.k_values)
+    
+     
+def compare_query_weather(retriever, corpus, queries, qrels):
+    # retriever.top_k = 2500
+    
+    results = retriever.retrieve(corpus, {"2": queries["2"], "3": queries["2"]})
+
+    print("length of the results::", len(results))
+
+    print("results without decomposition::")
+
+    ndcg, _map, recall, precision = retriever.evaluate({"2": qrels["2"], "3":qrels["2"]}, results, retriever.k_values)
+
+    # retriever.top_k = 1000
+
+    new_results = retriever.retrieve(corpus, {"2": ["change of weather", "coronaviruses"]})
+
+    #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
+    
+
+    new_merged_resuts = intersect_res(new_results)
+
+    print("length of the merged results::", len(new_merged_resuts))
+
+    print("results with decomposition::")
+
+    new_ndcg, new_map, new_recall, new_precision = retriever.evaluate({"2": qrels["2"], "3":qrels["2"]}, {"2": new_merged_resuts, "3":new_merged_resuts}, retriever.k_values)
+    
+    
+    new_results2 = retriever.retrieve(corpus, {"2": ["weather", "coronaviruses"]})    
+    
+    new_merged_resuts2 = intersect_res(new_results2)
+
+    print("length of the merged results::", len(new_merged_resuts2))
+
+    print("results with decomposition 2::")
+
+    new_ndcg2, new_map2, new_recall2, new_precision2 = retriever.evaluate({"2": qrels["2"], "3":qrels["2"]}, {"2": new_merged_resuts2, "3":new_merged_resuts2}, retriever.k_values)
+    
+def compare_query_social_distance(retriever, corpus, queries, qrels):
+    # retriever.top_k = 2500
+    
+    results = retriever.retrieve(corpus, {"10": queries["10"]})
+
+    print("length of the results::", len(results))
+
+    print("results without decomposition::")
+
+    ndcg, _map, recall, precision = retriever.evaluate({"10": qrels["10"]}, results, retriever.k_values)
+
+    # retriever.top_k = 1000
+
+    new_results = retriever.retrieve(corpus, {"10": ["social distancing", "impact", "slowing", "spread", "COVID-19"]})
+
+    #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
+    
+
+    new_merged_resuts = intersect_res(new_results)
+
+    print("length of the merged results::", len(new_merged_resuts))
+
+    print("results with decomposition::")
+
+    new_ndcg, new_map, new_recall, new_precision = retriever.evaluate({"10": qrels["10"]}, {"10": new_merged_resuts}, retriever.k_values)
+    
+def compare_ace_example(retriever, corpus, queries, qrels):
+    # retriever.top_k = 2500
+    
+    results = retriever.retrieve(corpus, {"20": queries["20"]})
+    
+    print("query::", queries["20"])
+
+    print("length of the results::", len(results))
+
+    print("results without decomposition::")
+
+    ndcg, _map, recall, precision = retriever.evaluate({"20": qrels["20"]}, results, retriever.k_values)
+
+    # retriever.top_k = 1000
+
+    new_results = retriever.retrieve(corpus, {"20": ["Angiotensin-converting enzyme inhibitors", "increased risk", "COVID-19"]})
+
+    #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
+    
+
+    new_merged_resuts = intersect_res(new_results)
+
+    print("length of the merged results::", len(new_merged_resuts))
+
+    print("results with decomposition::")
+
+    new_ndcg, new_map, new_recall, new_precision = retriever.evaluate({"20": qrels["20"]}, {"20": new_merged_resuts}, retriever.k_values)
+    
+    new_results = retriever.retrieve(corpus, {"20": ["patients", "Angiotensin-converting enzyme inhibitors", "increased risk", "COVID-19"]})
+
+    #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
+    
+
+    new_merged_resuts = intersect_res(new_results)
+
+    print("length of the merged results 2::", len(new_merged_resuts))
+
+    print("results with decomposition 2::")
+
+    new_ndcg, new_map, new_recall, new_precision = retriever.evaluate({"20": qrels["20"]}, {"20": new_merged_resuts}, retriever.k_values)
+    # new_results2 = retriever.retrieve(corpus, {"2": ["weather", "coronaviruses"]})    
+    
+    # new_merged_resuts2 = intersect_res(new_results2)
+
+    # print("length of the merged results::", len(new_merged_resuts2))
+
+    # print("results with decomposition 2::")
+
+    # new_ndcg2, new_map2, new_recall2, new_precision2 = retriever.evaluate({"2": qrels["2"], "3":qrels["2"]}, {"2": new_merged_resuts2, "3":new_merged_resuts2}, retriever.k_values)
+    
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+
+# data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data/")
+# dataset = "scifact"
+# url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
+dataset = "trec-covid"
+url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
+# out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "data")
+out_dir = "/data6/wuyinjun/beir/data/"
+data_path = util.download_and_unzip(url, out_dir)
+
+#### Provide the data_path where scifact has been downloaded and unzipped
+corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
+
+#### Load the SBERT model and retrieve using cosine-similarity
+model = DRES(models.SentenceBERT("msmarco-distilbert-base-tas-b"), batch_size=16)
+retriever = EvaluateRetrieval(model, score_function="cos_sim") # or "cos_sim" for cosine similarity
+
+
+# compare_query_social_distance(retriever, corpus, queries, qrels)
+
+compare_ace_example(retriever, corpus, queries, qrels)
+# compare_query_weather(retriever, corpus, queries, qrels)
+# compare_and_not_query(retriever, corpus, queries, qrels)
+
+# results = retriever.retrieve(corpus, {"2": queries["2"], "3": queries["2"]})
+
+# new_results = retriever.retrieve(corpus, {"51": "change of weather", "52":"coronaviruses"})
+
+# #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
+# print("results without decomposition::")
+
+# ndcg, _map, recall, precision = retriever.evaluate({"2": qrels["2"], "3":qrels["2"]}, results, retriever.k_values)
+
+# new_merged_resuts = intersect_res(new_results)
+
+# print("results with decomposition::")
+
+# new_ndcg, new_map, new_recall, new_precision = retriever.evaluate({"2": qrels["2"], "3":qrels["2"]}, {"2": new_merged_resuts, "3":new_merged_resuts}, retriever.k_values)
+
+
+print()
